12370c318a50f6f84193a4315af3036cc19f8ce7
Mario Gintili <mariojgintili@gmail.com>
2014-02-18 15:19:11 +0000

    added the first version of directory

---
+++ directory.rb
@@ -0,0 +1,34 @@
+# First we're going to print the list of our buddies
+puts "The students of my cohort at Makers Academy, We're February's cohort!"
+
+puts "-----------"
+
+puts "Mario Gintili"
+puts "Mikhail Dubov"
+puts "Karolis Noreika"
+puts "Michael Sidon"
+puts "Charles De Barros"
+puts "Ruslan Vikhor"
+puts "Toby Retallick"
+puts "Mark Mekhaiel"
+puts "Sarah Young"
+puts "Hannah Wight"
+puts "Khushkaran Singh"
+puts "Rick brunstedt"
+puts "Manjit Singh"
+puts "Alex Gaudiosi"
+puts "Ross Hepburn"
+puts "Natascia Marchese"
+puts "Tiffanie Chia"
+puts "Matthew Thomas"
+puts "Freddy McGroarty"
+puts "Tyler Rollins"
+puts "student 20"
+puts "student 21"
+puts "student 22"
+puts "student 23"
+# finally, we print the total
+print "Overall, we have "
+print 9
+puts " great students"
+
