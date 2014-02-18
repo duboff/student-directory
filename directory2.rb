d78b8e5b464192128d00a498f4158f7bb010e202
Mikhail Dubov <mdubov@gmail.com>
2014-02-18 22:18:27 +0000

    Ex3 - done

--- directory2.rb
+++ directory2.rb
@@ -5,7 +5,7 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    p "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    p "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0] == "A"
   end
 end
 
