f99befcaa277eb8ec5a661e8123ea2447614f969
Mario Gintili <mariojgintili@gmail.com>
2014-02-18 15:42:52 +0000

    black magic with subl

--- directory.rb
+++ directory.rb
@@ -1,32 +1,34 @@
 student_count = 23
 # First we're going to print the list of our buddies
-puts "The students of my cohort at Makers Academy, We're February's cohort!"
+puts  "The students of my cohort at Makers Academy,  We're February's cohort!"
 
 puts "-----------"
-
-puts "Mario Gintili"
-puts "Mikhail Dubov"
-puts "Karolis Noreika"
-puts "Michael Sidon"
-puts "Charles De Barros"
-puts "Ruslan Vikhor"
-puts "Toby Retallick"
-puts "Mark Mekhaiel"
-puts "Sarah Young"
-puts "Hannah Wight"
-puts "Khushkaran Singh"
-puts "Rick brunstedt"
-puts "Manjit Singh"
-puts "Alex Gaudiosi"
-puts "Ross Hepburn"
-puts "Natascia Marchese"
-puts "Tiffanie Chia"
-puts "Matthew Thomas"
-puts "Freddy McGroarty"
-puts "Tyler Rollins"
-puts "Richard Curteis"
-puts "Anna"
-puts "Mistery Man"
-# finally, we print the total
-print "Overall, we have #{student_count} great students!"
+students = [
+ "Mario Gintili",
+ "Mikhail Dubov",
+ "Karolis Noreika",
+ "Michael Sidon",
+ "Charles De Barros",
+ "Ruslan Vikhor",
+ "Toby Retallick",
+ "Mark Mekhaiel",
+ "Sarah Young",
+ "Hannah Wight",
+ "Khushkaran Singh",
+ "Rick brunstedt",
+ "Manjit Singh",
+ "Alex Gaudiosi",
+ "Ross Hepburn",
+ "Natascia Marchese",
+ "Tiffanie Chia",
+ "Matthew Thomas",
+ "Freddy McGroarty",
+ "Tyler Rollins",
+ "Richard Curteis",
+ "Anna",
+ "Mistery Man",
+]
+# finally,  we print the total
+puts students
+print "Overall,  we have #{student_count} great students!"
 
