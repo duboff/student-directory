115f5d212336828e1bd41f22f929dca4210fe351
Mikhail Dubov <mdubov@gmail.com>
2014-02-19 10:09:00 +0000

    ex. 6

--- directory2.rb
+++ directory2.rb
@@ -6,7 +6,7 @@ end
 def print(students)
   n = 0
   while n < students.length
-    puts "#{students[n][:name]} (#{students[n][:cohort]} cohort)" if students[n][:name].length < 12
+    puts "#{students[n][:name]}, #{students[n][:hobby]}, #{students[n][:country]} (#{students[n][:cohort]} cohort)"
     n += 1
   end
 
@@ -20,19 +20,25 @@ def print_footer(names)
 end
 
 def input_students
-  p "Please enter the names of the students"
+  p "Please enter the name, hobby and country of birth of each ofthe students"
+  p "Use commas to separate names, hobbies and country of birth i.e. Michael, snowboarding, Russia."
+  p 'Hit return to enter a new name'
   p "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  student = gets.chomp.split(', ')
+
   # while the name is not empty, repeat this code
-  while !name.empty? do    
+  while !student.empty? do    
     # add the student hash to the array
-    students << {:name => name, :cohort => :november}    
+    students << {:name => student[0],
+                  :hobby => student [1],
+                  :country => student [2],
+                  :cohort => :february}  
     p "Now we have #{students.length} students"
     # get another name from the user
-    name = gets.chomp
+    student = gets.chomp.split(', ')
   end
   # return the array of students
   students
